77b33942ca68b13e050a203971618ebfaa078376
djtfmartin <djtfmartin@gmail.com>
2012-11-20 02:42:57 +0000

    user list
    git-svn-id: http://ala-mobile.googlecode.com/svn/trunk/userdetails@243 6a6df900-09af-505f-f2bd-838eb8a94856

--- grails-app/controllers/userdetails/UserDetailsController.groovy
+++ grails-app/controllers/userdetails/UserDetailsController.groovy
@@ -2,7 +2,8 @@ package userdetails
 
 class UserDetailsController {
 
-    static allowedMethods = [getUserDetails: "POST"]
+    static allowedMethods = [getUserDetails: "POST", getUserByIdOrEmail: "GET", getUserList: "POST", getUserListWithIds: "POST"]
+    def authorisedSystemService
 
     def getUserDetails() { 
         println("Getting details for username: " + params.userName)
@@ -10,10 +11,45 @@ class UserDetailsController {
         User user = User.findByUserName(params.userName)
         if (user == null) {
             println("No record for username:" + params.userName)
-            response.sendError(403)
+            response.sendError(404)
         } else {
             println("Username: " + user.userName + ", First name: " + user.firstName + ", Last name: " + user.lastName)
             [userName: user.userName, firstName: user.firstName, lastName: user.lastName]
+        }
+    }
+
+    def getUserByIdOrEmail(){
+        User user = User.findByUserNameOrId(params.q)
+        if (user == null) {
+            println("No record for username:" + params.userName)
+            response.sendError(404)
+        } else {
+            println("Username: " + user.userName + ", First name: " + user.firstName + ", Last name: " + user.lastName)
+            render(contentType: "text/json"){
+                [userName: user.userName, firstName: user.firstName, lastName: user.lastName]
+            }
+        }
+    }
+
+    def getUserList(){
+        if(authorisedSystemService.isAuthorisedSystem(request)){
+            def users = User.findAll()
+            def map = [:]
+            users.each { map.put(it.email.toLowerCase(), it.firstName + " "+ it.lastName) }
+            render(contentType: "text/json"){ map }
+        } else {
+            response.sendError(403)
+        }
+    }
+
+    def getUserListWithIds(){
+        if(authorisedSystemService.isAuthorisedSystem(request)){
+            def users = User.findAll()
+            def map = [:]
+            users.each { map.put(it.id, it.firstName + " "+ it.lastName) }
+            render(contentType: "text/json"){ map }
+        } else {
+            response.sendError(403)
         }
     }
 }
