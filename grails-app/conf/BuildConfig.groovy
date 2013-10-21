782cb431c7d4eab55aae54bd7cd7836e10eb64f8
djtfmartin <djtfmartin@gmail.com>
2013-10-21 23:06:47 +0000

    updated the resources plugins and specified spock 0.7
    git-svn-id: http://ala-mobile.googlecode.com/svn/trunk/userdetails@583 6a6df900-09af-505f-f2bd-838eb8a94856

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,8 +44,9 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
-        runtime ":resources:1.1.5"
+        runtime ":resources:1.2.1"
         runtime ":ala-web-theme:0.1.12"
+        test ":spock:0.7"
         build ":tomcat:$grailsVersion"
     }
 }
