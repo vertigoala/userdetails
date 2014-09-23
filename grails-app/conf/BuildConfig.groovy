ab7097390bd57fdb6cbc69320e001ef29a81e184
Bear, Simon (CSIRO IM&T, Yarralumla) <Simon.Bear@csiro.au>
2014-09-23 17:56:35 +1000

    Update ala-web-theme to v0.8.1 Update default_config to be added to Grails config holder Update application.properties to reflect actual grails version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,7 +48,7 @@ grails.project.dependency.resolution = {
 //        runtime ":jquery:1.7.1"
         runtime ":resources:1.2.1"
        // build ":release:3.0.1"
-        runtime ":ala-web-theme:0.2.4"
+        runtime ":ala-web-theme:0.8.1"
         compile ":csv:0.3.1"
 //        test ":spock:0.7"
         build ":tomcat:$grailsVersion"
