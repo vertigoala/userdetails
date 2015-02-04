0b64cfebb28acd768197bf581af1c9f3a4bccecc
m-r-c <m-r-c@users.noreply.github.com>
2015-02-04 11:15:55 +1100

    Adds datasource configuration properties

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -3,6 +3,29 @@ dataSource {
     driverClassName = "com.mysql.jdbc.Driver"
     username = ""
     password = ""
+    properties {
+        // http://www.grails.org/doc/latest/guide/single.html#dataSource
+        // see http://tomcat.apache.org/tomcat-7.0-doc/jdbc-pool.html#Common_Attributes for more
+        timeBetweenEvictionRunsMillis = 60000 // milliseconds (default = 5000)
+        testOnBorrow = true // default = false
+        testOnReturn = false // default = false
+        testWhileIdle = true // default = false
+        validationQuery = "SELECT 1" // default = null
+        validationQueryTimeout = 10 //seconds (default = -1 i.e. disabled)
+        removeAbandoned = true // // default = false
+        removeAbandonedTimeout = 300 // seconds (default = 60)
+        logAbandoned = true // adds some overhead to every borrow from the pool, disable if it becomes a performance issue
+        logValidationErrors = true // default = false, logs validation errors at SEVERE
+
+        // mysql jdbc connection properties - see http://dev.mysql
+        // .com/doc/connector-j/en/connector-j-reference-configuration-properties.html
+        dbProperties {
+            autoReconnect = true
+            connectTimeout = 0
+            useUnicode = true
+            characterEncoding = "UTF-8"
+        }
+    }
 }
 hibernate {
     cache.use_second_level_cache = true
