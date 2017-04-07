d0442fa7a7d3c618e2dce9a0892327181279b60d
Patricia Koh <patricia.koh@csiro.au>
2017-04-07 16:54:56 +1000

    Remove comment

--- grails-app/controllers/au/org/ala/userdetails/RegistrationController.groovy
+++ grails-app/controllers/au/org/ala/userdetails/RegistrationController.groovy
@@ -181,7 +181,6 @@ class RegistrationController {
         if (user.tempAuthKey == params.authKey) {
             userService.activateAccount(user)
             render(view: 'accountActivatedSuccessful', model: [user: user])
-            //redirect(url: grailsApplication.config.security.cas.loginUrl + "?email=" + user.email + "&service=" + grailsApplication.config.redirectAfterFirstLogin)
         } else {
             log.error('Auth keys did not match for user : ' + params.userId + ", supplied: " + params.authKey + ", stored: " + user.tempAuthKey)
             render(view: "accountError")
