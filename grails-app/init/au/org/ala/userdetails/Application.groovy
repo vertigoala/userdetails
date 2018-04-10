4f21a9da657fcd75be32ce174fb6d15c69342251
Simon Bear <simon.bear@csiro.au>
2018-04-10 16:49:26 +1000

    Disable redis CONFIG action (for eg elasticache) based on config value

--- grails-app/init/au/org/ala/userdetails/Application.groovy
+++ grails-app/init/au/org/ala/userdetails/Application.groovy
@@ -3,11 +3,21 @@ package au.org.ala.userdetails
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import groovy.util.logging.Slf4j
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.session.data.redis.config.ConfigureRedisAction
 
 @Slf4j
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+    }
+
+    @ConditionalOnProperty('spring.session.disable-redis-config-action')
+    @Bean
+    ConfigureRedisAction configureRedisAction() {
+        ConfigureRedisAction.NO_OP
     }
 
 }
