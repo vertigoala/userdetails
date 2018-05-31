98f8db5973e0f6df908fcd8d75013814cb70dc76
Simon Bear <simon.bear@csiro.au>
2018-05-31 20:51:33 +1000

    Fix imports

--- grails-app/services/au/org/ala/userdetails/LocationService.groovy
+++ grails-app/services/au/org/ala/userdetails/LocationService.groovy
@@ -5,10 +5,8 @@ import grails.plugin.cache.Cacheable
 import grails.transaction.NotTransactional
 import org.grails.web.json.JSONObject
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.Resource
-import org.springframework.core.io.ResourceLoader
 
 class LocationService {
 
